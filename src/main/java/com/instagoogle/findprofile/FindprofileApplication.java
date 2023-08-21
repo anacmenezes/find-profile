051e25627490363bccc112005e7db184dededa39
anacmezes <anamenezes.dev@gmail.com>
2023-08-21 10:37:11 -0300

    Project created

---
+++ src/main/java/com/instagoogle/findprofile/FindprofileApplication.java
@@ -0,0 +1,13 @@
+package com.instagoogle.findprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindprofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindprofileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instagoogle/findprofile/FindprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instagoogle.findprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
