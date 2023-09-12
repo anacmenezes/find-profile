88f29108fec913f8217f633020d7191fba000b07
anacmezes <anamenezes.dev@gmail.com>
2023-09-12 09:37:51 -0300

    Made CRUD for category

--- src/main/java/com/instagoogle/findprofile/resources/CategoryResource.java
+++ src/main/java/com/instagoogle/findprofile/resources/CategoryResource.java
@@ -1,27 +1,66 @@
 package com.instagoogle.findprofile.resources;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.instagoogle.findprofile.domain.Category;
+import com.instagoogle.findprofile.dto.CategoryDTO;
+import com.instagoogle.findprofile.services.CategoryService;
+
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping(value ="/categories")
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Category> find(@PathVariable Integer id) {
+		Category obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoryDTO objDto) {
+		Category obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoryDTO objDto, @PathVariable Integer id) {
+		Category obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public List<Category> listing() {
-		
-		Category cat1 = new Category(1, "Mães");
-		
-		List<Category> table = new ArrayList<>();
-		table.add(cat1);
-		
-		return table;
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<Category> list = service.findAll();
+		List<CategoryDTO> listDto = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/instagoogle/findprofile/services/CategoryService.java
+++ src/main/java/com/instagoogle/findprofile/services/CategoryService.java
@@ -1,8 +1,56 @@
 package com.instagoogle.findprofile.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import com.instagoogle.findprofile.domain.Category;
+import com.instagoogle.findprofile.dto.CategoryDTO;
+import com.instagoogle.findprofile.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repo;
 
+	public Category find(Integer id) {
+		Optional<Category> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+	public Category insert(Category obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Category update(Category obj) {
+		Category newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw (null);
+		}
+	}
+	
+	public List<Category> findAll() {
+		return repo.findAll();
+	}	
+	
+	public Category fromDTO(CategoryDTO objDto) {
+		return new Category(objDto.getId(), objDto.getName());
+	}
+
+	private void updateData(Category newObj, Category obj) {
+		newObj.setName(obj.getName());
+	}
 }
